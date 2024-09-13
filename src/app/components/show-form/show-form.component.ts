432c67795c45f5883e4dfad267c2ef824ce0a6fe
juanQNav <juan.qn363@gmail.com>
2024-09-12 18:23:33 -0600

    go to first good version

--- src/app/components/show-form/show-form.component.ts
+++ src/app/components/show-form/show-form.component.ts
@@ -1,5 +1,5 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Show } from '../../interfaces/show.interface';
 
@@ -11,53 +11,27 @@ import { Show } from '../../interfaces/show.interface';
   styleUrl: './show-form.component.css'
 })
 export class ShowFormComponent {
-  //show to edit
-  @Input()
-  public showToEdit: Show | null = null;
-
-  //create element
   @Output()
   public createElement: EventEmitter<Show> = new EventEmitter();
 
-  //update element
-  @Output()
-  public updateElement: EventEmitter<Show> = new EventEmitter();
-
-  // Properties to handle form values
-  public name: string = ''
-  public description: string = ''
-  public image: string = ''
-
-  ngOnInit(): void {
-    console.log('showToEdit:', this.showToEdit);
-    if (this.showToEdit) {
-      this.name = this.showToEdit.name;
-      this.description = this.showToEdit.description;
-      this.image = this.showToEdit.image;
-    }
-  }
-
-  public onFormSubmit(form: NgForm): void {
-    if (form.valid) {
+  public onFormSubmit(form: NgForm): void{
+    
+    if(form.valid){    
       const newShow: Show = {
-        description: this.description,
+        description: form.value.description,
         episodes: 0,
         genre: "",
-        image: this.image,
+        image: form.value.image,
         likes: [],
-        name: this.name,
+        name: form.value.name,
         year: 0
-      };
-
-      if (this.showToEdit) {
-        this.updateElement.emit(newShow);
-      } else {
-        this.createElement.emit(newShow);
       }
-
+      //Emitir el evento al padre y pasarle el nuevo show a insertar
+      this.createElement.emit(newShow);
       form.resetForm();
-    } else {
-      console.log("Faltan datos");
+    }else{
+      console.log("Falantan datos");
     }
   }
+
 }
